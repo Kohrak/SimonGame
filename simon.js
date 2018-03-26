bfa2f4426b753f41e36ce3b81314851c8248912a
Kohrak <Gabriel@PPM.local>
2018-03-26 11:49:21 +0100

    add limit functionality

--- simon.js
+++ simon.js
@@ -9,9 +9,9 @@ var strictButton = document.querySelector("#strictb")
 var running = false;
 var score = 0
 var strict = false;
-
+var limit = 2;
 var sounds = [
-  new Howl({src: ['sounds/dotted-spiral.mp3']}),
+  new Howl({src: ['sounds/ufo.mp3']}),
   new Howl({src: ['sounds/suspension.mp3']}),
   new Howl({src: ['sounds/moon.mp3']}),
   new Howl({src: ['sounds/zig-zag.mp3']})
@@ -57,12 +57,13 @@ function displayScore(){
 }
 
 function playerMove(num){
+  sounds[num].play();
   playerSequence.push(num);
   var ind = playerSequence.length - 1
   if (playerSequence[ind] != sequence[ind]){
     if(strict){
       alert("you will have to start from the start");
-      restart();
+      setTimeout(restart, 2000);
     } else {
       msg.textContent = "wrong, watch and start over";
       playerSequence = [];
@@ -71,10 +72,15 @@ function playerMove(num){
   } else {
     if (ind == sequence.length - 1){
       msg.textContent = "correct!"
-      playerSequence = [];
-      turn();
       score ++
       displayScore();
+      if (score == limit){
+        alert("you win!");
+        setTimeout(restart, 2000);
+      } else {
+        playerSequence = [];
+        turn();
+      }
     }
   }
 }
@@ -92,7 +98,6 @@ function restart(){
 squares.forEach(function(square){
   square.addEventListener("click", function(){
     if (!running && sequence.length > 0){
-      sounds[this.id].play();
       playerMove(this.id);
     }
   })
