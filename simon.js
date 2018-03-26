7655fd666ae1894eb0bba7a0669a8f47b627d34c
Kohrak <Gabriel@PPM.local>
2018-03-26 08:19:29 +0100

    add functionality to display a move

--- simon.js
+++ simon.js
@@ -1,5 +1,6 @@
 var sequence = []
 var playerSequence = []
+var squares = document.querySelectorAll(".square");
 function newMove(num){
   return Math.floor((Math.random() * num));
 }
@@ -9,4 +10,11 @@ function turn(){
   sequence.push(newMove(4));
   //display moves
   console.log(sequence)
+}
+
+function displayMove(square){
+  square.classList.toggle("selected");
+  setTimeout(function(){
+    square.classList.toggle("selected");
+  }, 4000);
 }
