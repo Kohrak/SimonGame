ed13ac3fa4bfdc3718a46b0385cc18a4807a05fb
Kohrak <Gabriel@PPM.local>
2018-03-26 08:02:41 +0100

    add generate sequence functionality

--- simon.js
+++ simon.js
@@ -1,1 +1,12 @@
-alert("connected");
+var sequence = []
+var playerSequence = []
+function newMove(num){
+  return Math.floor((Math.random() * num));
+}
+
+function turn(){
+  //generate new move
+  sequence.push(newMove(4));
+  //display moves
+  console.log(sequence)
+}
